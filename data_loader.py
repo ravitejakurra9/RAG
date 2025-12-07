6bcbaa436263adb5dbb58058d99a65f516443fc7
ravitejakurra9 <ravitejakurra9@gmail.com>
2025-12-07 20:37:31 +0530

    data loader class

---
+++ data_loader.py
@@ -0,0 +1,22 @@
+import pathlib
+
+class DataLoader:
+
+    def __init__(source_dir:str, file_types:list):
+        self.source_dir = source_dir
+        self file_types: = file_types
+    
+    def load_files():
+        pathlib.glob()
+    
+    def load_excel_files():
+        pass
+    
+    def load_pdf_files():
+        pass
+    
+    def load_csv_files():
+        pass
+    
+    def load_doc_files():
+        pass
